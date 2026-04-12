840661554ff11c2660df6b7141a3c1c3db6e2e0b
gituser-30 <aryanmandhare30@gmail.com>
2026-04-12 23:18:39 +0530

    Optimize Render deployment: fix networking, ports, and headers

--- server/index.js
+++ server/index.js
@@ -27,7 +27,6 @@ connectDB();
 app.use(cors());
 app.use(express.json({ limit: '50mb' }));
 app.use((req, res, next) => {
-  res.header("Cross-Origin-Opener-Policy", "same-origin-allow-popups");
   next();
 });
 
